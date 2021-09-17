24a350306cf633447c35866fc6fd28fde608ac44
Ahmet Yusuf Sirin <mnksgames@gmail.com>
2021-09-17 08:07:04 +0300

    Initial commit

---
+++ src/Consumer.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.Random;
+
+class Consumer implements Runnable {
+    private List<String> buffer;
+
+    public Consumer(List<String> buffer) {
+        this.buffer = buffer;
+    }
+
+    @Override
+    public void run() {
+
+        while (true) {
+            synchronized (buffer) {
+                if (buffer.isEmpty()) {
+                    continue;
+                }
+                if (buffer.get(0).equals(Main.EOF)) {
+                    System.out.println(Thread.currentThread().getName() + " exiting.");
+                    break;
+                } else {
+                    System.out.println(Thread.currentThread().getName() + " removed " + buffer.remove(0));
+                    try {
+                        Random random = new Random();
+                        Thread.sleep(random.nextInt(2000));
+                    } catch (InterruptedException e) {
+                        System.out.println(Thread.currentThread().getName() + " interrupted.");
+                    }
+                }
+            }
+        }
+    }
+}
